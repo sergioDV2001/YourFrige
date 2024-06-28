4208612e3a7379d1357a7f3a1019d9e37a5853f0
Sergio Dominguez <servarela2001@gmail.com>
2024-06-29 00:22:27 +0200

    feat: created first layout

---
+++ src/shared/icons/MailIcon.tsx
@@ -0,0 +1,24 @@
+import { SVGProps } from "react";
+
+export function MailIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1.4em"
+      height="1.4em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+      >
+        <path d="M3 7a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+        <path d="m3 7l9 6l9-6" />
+      </g>
+    </svg>
+  );
+}
